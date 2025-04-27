3edc16a57dfbdaade3b6865b3e33561c1d1d128f
Drew <tullos.andrew@gmail.com>
2025-04-27 11:58:25 -0500

    Seeds updated

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,23 +1,32 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Thought, Diary, Like, Comment, Follow } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const thoughtSeeds = require('./thoughtSeeds.json');
+const diarySeeds = require('./diarySeeds.json');
+const likeSeeds = require('./likeSeeds.json');
+const commentSeeds = require('./commentSeeds.json');
+const followSeeds = require('./followSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
+    await cleanDB('Follow', 'follows');
+    await cleanDB('Comment', 'comments');
+    await cleanDB('Like', 'likes');
+    await cleanDB('Diary', 'diaries');
     await cleanDB('Thought', 'thoughts');
     await cleanDB('User', 'users');
 
     const createdUsers = await User.create(userSeeds);
 
+    const createdThoughts = [];
     for (const thought of thoughtSeeds) {
       const user = createdUsers.find(
           (user) => user.username === thought.thoughtAuthor
       );
 
       if (!user) {
-        console.error(`Could not find user: ${thought.thoughtAuthor}`);
+        console.error(`Could not find user: ${thought.thoughtAuthor} for thought: ${thought.thoughtText}`);
         continue;
       }
 
@@ -25,6 +34,7 @@ db.once('open', async () => {
         thoughtText: thought.thoughtText,
         thoughtAuthor: user._id,
       });
+      createdThoughts.push(createdThought);
 
       await User.findOneAndUpdate(
           { _id: user._id },
@@ -35,11 +45,52 @@ db.once('open', async () => {
           }
       );
     }
+
+    const userMap = createdUsers.reduce((acc, user) => {
+      acc[user.username] = user._id;
+      return acc;
+    }, {});
+
+    if (diarySeeds && diarySeeds.length > 0) {
+      const createdDiaries = await Diary.create(diarySeeds.map(diary => ({
+        ...diary,
+        diaryAuthor: userMap[diary.diaryAuthor],
+      })));
+    }
+
+    if (likeSeeds && likeSeeds.length > 0) {
+      const createdLikes = await Like.create(likeSeeds.map(like => ({
+        likeAuthor: userMap[like.likeAuthor],
+        thought: createdThoughts.find(thought => thought.thoughtText.startsWith(like.thought))?._id,
+        createdAt: like.createdAt,
+      })).filter(like => like.thought));
+    }
+
+    if (commentSeeds && commentSeeds.length > 0) {
+      const createdComments = await Comment.create(commentSeeds.map(comment => ({
+        thought: createdThoughts.find(thought => thought.thoughtText.startsWith(comment.thought))?._id,
+        commentText: comment.commentText,
+        commentAuthor: userMap[comment.commentAuthor],
+        createdAt: comment.createdAt,
+      })).filter(comment => comment.thought));
+    }
+
+    if (followSeeds && followSeeds.length > 0) {
+      const createdFollows = await Follow.create(followSeeds.map(follow => ({
+        follower: userMap[follow.follower],
+        following: userMap[follow.following],
+      })));
+
+      for (const follow of createdFollows) {
+        await User.findByIdAndUpdate(follow.follower, { $addToSet: { following: follow.following } });
+        await User.findByIdAndUpdate(follow.following, { $addToSet: { followers: follow.follower } });
+      }
+    }
+
+    console.log('all done!');
+    process.exit(0);
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
-
-  console.log('all done!');
-  process.exit(0);
 });
