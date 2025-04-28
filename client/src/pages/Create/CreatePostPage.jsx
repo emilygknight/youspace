7829370ce77fe28908ad3e6850f2e88b25352892
Drew <tullos.andrew@gmail.com>
2025-04-28 16:42:01 -0500

    package.json adds

---
+++ client/src/pages/Create/CreatePostPage.jsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Badge } from "@/components/ui/badge"
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
+import {
+    ArrowLeft,
+    AtSign,
+    Camera,
+    ChevronDown,
+    GiftIcon as Gif,
+    Globe,
+    ImageIcon,
+    Loader2,
+    Lock,
+    MapPin,
+    Smile,
+    Sparkles,
+    Users,
+    X,
+} from "lucide-react"
+
+
+function PostPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default PostPage;
